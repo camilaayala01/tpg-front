6b332f2b5de33642da3a3e1832deb79ed0a42253
Manuel <jpol@fi.uba.ar>
2023-12-01 12:42:45 -0300

    conexion con backend soporte, para GET tickets

--- services/support/fetcher.ts
+++ services/support/fetcher.ts
@@ -1,1 +1,21 @@
-export const supportAPIUrl = "http://localhost:8080/swagger-ui.html#/memo-1-tpg"
+export const supportAPIUrl = "http://localhost:8080"
+
+export const postHeaders = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  };
+
+const genericFetcher = (
+baseUrl: string,
+resource: string,
+options?: RequestInit
+) =>
+fetch(baseUrl + resource, options)
+    .then((res) => {
+    if (res.status >= 400) throw new Error(`Error code: ${res.status}`);
+    return res;
+    })
+    .then((res) => res?.json());
+
+export const supportFetcher = (resource: string, options?: RequestInit) =>
+genericFetcher(supportAPIUrl, resource, options);
