ff919e6cb88ed728514c2fdacbf1b1648c508caf
noahmasri <mmasri@fi.uba.ar>
2023-12-01 13:02:12 -0300

    cortamos un rato

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -10,7 +10,6 @@ export function HeaderItem({ title }: { title: string }) {
   return <th className="px-6 py-3 text-sm text-left text-gray-500 border-b border-gray-200 bg-gray-50">{title}</th>
 }
 
-
 function handleButtonClick() {
   const router = useRouter();
   router.push(`create`);
