1ed8ed98c5eacf9e52ec79aa6a3a55674823f823
V-SANT <vsantander@fi.uba.ar>
2023-12-03 02:51:30 -0300

    Vamos que se puede :,)

--- components/support/TextBox.tsx
+++ components/support/TextBox.tsx
@@ -4,26 +4,26 @@ interface CampoDeTextoProps {
   style?: React.CSSProperties;  // Define la propiedad opcional 'style'
 }
 
-const TextBox: React.FC<{label: any; description: any; style?: React.CSSProperties}> = ({ label, description, style }) => {
-  // Utilizamos el estado para manejar el valor del campo de texto
+const TextBox: React.FC<{ label: any; description: any; style?: React.CSSProperties; onChange: (name: string, value: string) => void }> = ({ label, description, style, onChange }) => {
   const [valor, setValor] = useState('');
 
-  // Manejar cambios en el campo de texto
-  const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-    setValor(event.target.value);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setValor(value);
+    onChange(value, value);
   };
 
   return (
     <div style={style}>
-        <p style={{fontWeight: 'bold', color: 'black'}}>{label}</p>
-        <p style={{fontSize: '0.9rem', color: 'black'}}>{description}</p>
-        <input
-            type="text"
-            id="campoTexto"
-            value={valor}
-            onChange={handleChange}
-            style={{backgroundColor: '#EDEDED', width: '300px', height: '40px', borderRadius: '12px', color: 'black'}}
-        />
+      <p style={{ fontWeight: 'bold', color: 'black' }}>{label}</p>
+      <p style={{ fontSize: '0.9rem', color: 'black' }}>{description}</p>
+      <input
+        type="text"
+        id={`campoTexto_${label}`}
+        value={valor}
+        onChange={handleChange}
+        style={{ backgroundColor: '#EDEDED', width: '300px', height: '40px', borderRadius: '12px', color: 'black' }}
+      />
     </div>
   );
 };
